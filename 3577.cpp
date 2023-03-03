6bb72cf3c1b01496ac2a0bdddb89a1aae731a3c0
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-03-03 18:58:56 -0500

    Update 3577.cpp

--- 3577.cpp
+++ 3577.cpp
@@ -1,1 +1,44 @@
-ccc
+#include <iostream>
+#include <vector>
+
+using namespace std;  
+
+int n, m;
+void solve() {
+    vector<int> a, b;
+    vector<bool> exist(201, false);
+    cin >> n; //a size
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
+        exist[x] = true;
+    }
+    
+    cin >> m; //b size
+    for(int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        b.push_back(x);
+        exist[x] = true;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            int sum = a[i] + b[j];
+            //sum not in a and not in b
+            //exist[sum] = false  => not in a and not in b
+            if(!exist[sum]) {
+                cout << a[i] << " " << b[j] << endl;
+                return;
+            }
+        }
+    }
+    
+    cout << -1 << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
